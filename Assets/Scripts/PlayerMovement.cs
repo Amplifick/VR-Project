e2091c67c9d9bf243a0498969f0adb2f5c7bed07
Jaime Andres <jaime.artunduaga@uao.edu.co>
2020-12-03 22:01:19 -0500

    PlayerMovement
    Scripts player Movement, JoyButton

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    protected Joystick joystick;
+    protected JoyButton joybutton;
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        joybutton = FindObjectOfType<JoyButton>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.velocity = new Vector3(joystick.Horizontal * 100f, rigidbody.velocity.y, joystick.Vertical * 100f);
+    }
+}
