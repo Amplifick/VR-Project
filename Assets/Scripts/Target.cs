6e9cc77aa1a787c1346e99da556ace22e3d84689
MadeColombia <MadeColombia@users.noreply.github.com>
2020-12-04 15:18:49 -0500

    Shooting p1
    method already working with the zombies

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float vida = 50f;
+
+    public void TakeDamage (float amount)
+    {
+        vida -= amount;
+        if(vida<= 0)
+        {
+            Muere();
+        }
+    }
+
+    void Muere()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/TouchJoystickRotation.cs
@@ -0,0 +1,56 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchJoystickRotation : MonoBehaviour
+{
+	public Joystick joystick;
+	public GameObject Object;
+	Vector2 GameobjectRotation;
+	private float GameobjectRotation2;
+	public float daño = 10f;
+	public float rango = 100f;
+
+
+	public bool FacingRight = true;
+	
+	void Update()
+	{
+		//Gets the input from the jostick
+		GameobjectRotation = new Vector2(joystick.Horizontal, joystick.Vertical);
+
+		
+		GameobjectRotation2 = GameobjectRotation.x + GameobjectRotation.y * 360;
+		Object.transform.rotation = Quaternion.Euler(0f, GameobjectRotation2, 0f);
+
+
+		if (joystick.isActiveAndEnabled)
+		{
+
+			disparo();
+
+		}
+		
+		
+		
+		
+	}
+
+	void disparo()
+	{
+		RaycastHit hit;
+		if(Physics.Raycast(Object.transform.position, Object.transform.forward, out hit, rango))
+			{
+
+			Target target = hit.transform.GetComponent<Target>();
+			if(target != null)
+			{
+				target.TakeDamage(daño);
+			}
+		}
+
+	}
+
+
+}
